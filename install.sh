437d0bd1cd484fa7fa46fbcf5fb10fe86b7118a4
Emerica <blendz@shaw.ca>
2019-06-26 12:49:50 -0600

    Create installer

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+#Install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#Install OSX Fuse
+brew cask install osxfuse
+#install Judy and GCC6
+brew install traildb/judy/judy gcc@6 wget
+#Download LizardFS
+wget -O lizardfs.pkg https://github.com/Emerica/lizardfs_osx/blob/master/lizardfs.pkg?raw=true
+#install the package
+sudo installer -pkg lizardfs.pkg -target /
+
+
